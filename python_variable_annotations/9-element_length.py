4e0df21cd6704241966af5ff6fa63fb27da06391
CarolinaMolinaSosa <Molinasosacarolina17@gmail.com>
2023-08-28 10:58:59 -0700

    como que no

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Task 9"""
 
+from typing import List, Tuple, Union, Callable, Iterable, Sequence
 
-def element_length(lst: list[str]) -> list[tuple[str, int]]:
-    """
-    Calculate the length of each element in the list
-    """
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Calculate the length of each element in the list"""
+
     return [(i, len(i)) for i in lst]
